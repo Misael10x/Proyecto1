765a0acbff798c774ad6ee4e71d30ff94d99ecf8
Misael10x <misabar12389@gmail.com>
2026-03-17 15:39:50 -0600

    update 2026-03-17 commit 1

--- commits_historial.sh
+++ commits_historial.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
+# fecha inicial (ajústala si quieres otro mes)
+start_date="2026-03-17"
+
+# hoy
+end_date=$(date +%Y-%m-%d)
+
 file="actividad.txt"
 
-# fechas
-today=$(date +%Y-%m-%d)
-yesterday=$(date -d "yesterday" +%Y-%m-%d)
+current=$start_date
 
-for day in $yesterday $today
+while [[ "$current" < "$end_date" ]] || [[ "$current" == "$end_date" ]]
 do
 
-# entre 5 y 8 commits
-num=$((5 + RANDOM % 4))
+# entre 15 y 20 commits
+num=$((15 + RANDOM % 6))
 
 for ((i=1;i<=num;i++))
 do
 
-hour=$((10 + RANDOM % 10))
+hour=$((9 + RANDOM % 10))
 minute=$((RANDOM % 60))
 second=$((RANDOM % 60))
 
-echo "actividad $day $i" >> $file
+echo "actividad $current $i $(date +%s)" >> $file
 
 git add .
 
-GIT_AUTHOR_DATE="$day $hour:$minute:$second" \
-GIT_COMMITTER_DATE="$day $hour:$minute:$second" \
-git commit -m "update $day commit $i"
+GIT_AUTHOR_DATE="$current $hour:$minute:$second" \
+GIT_COMMITTER_DATE="$current $hour:$minute:$second" \
+git commit -m "update $current commit $i"
 
 done
+
+current=$(date -I -d "$current + 1 day")
 
 done
 
